6a28ef6e5bf5885a42032f01e0347729e0e12a1a
Rishiraj Anand <rishiraj.anand1@gmail.com>
2021-01-08 16:57:49 +0530

    adasa

---
+++ build/precache-manifest.61664740ad917c6695e2779d23f6cdc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eea3ddd3fe8a49437e9a3b24d1433a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af08cad55fcb2cd21760",
+    "url": "/static/css/2.10bcc4b6.chunk.css"
+  },
+  {
+    "revision": "131a82eea96e54c7464b",
+    "url": "/static/css/main.c4cdc86d.chunk.css"
+  },
+  {
+    "revision": "af08cad55fcb2cd21760",
+    "url": "/static/js/2.ef61d314.chunk.js"
+  },
+  {
+    "revision": "164fd2d2ef0611c2971584a9bdeaa94a",
+    "url": "/static/js/2.ef61d314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131a82eea96e54c7464b",
+    "url": "/static/js/main.d0e0f2c2.chunk.js"
+  },
+  {
+    "revision": "32f52754b6c1f2e84842",
+    "url": "/static/js/runtime-main.8be7c7f3.js"
+  },
+  {
+    "revision": "e8f986aca7943f95a083617c369d4be8",
+    "url": "/static/media/logo.e8f986ac.jpg"
+  }
+]);
