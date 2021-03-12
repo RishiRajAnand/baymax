590d23867579b0217a5fccfa8b3e786c0be7b7f6
RishiRajAnand <rishiraj.anand1@gmail.com>
2021-03-12 11:25:57 +0530

    added GST in inventory

---
+++ build/precache-manifest.e2c180574168f31cbfbacce93f91b774.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d622b98f4688f6ef4ff89fb8106933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3345c3607d7255fc6936",
+    "url": "/static/css/2.471b51ca.chunk.css"
+  },
+  {
+    "revision": "78c1803187a1de848ed8",
+    "url": "/static/css/main.ee82e8eb.chunk.css"
+  },
+  {
+    "revision": "3345c3607d7255fc6936",
+    "url": "/static/js/2.a089014c.chunk.js"
+  },
+  {
+    "revision": "2f142915b74687ef5d09a3271aa85e26",
+    "url": "/static/js/2.a089014c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c1803187a1de848ed8",
+    "url": "/static/js/main.c9b9f4dd.chunk.js"
+  },
+  {
+    "revision": "32f52754b6c1f2e84842",
+    "url": "/static/js/runtime-main.8be7c7f3.js"
+  },
+  {
+    "revision": "05e570b3d2fc918c410abed98cc84d50",
+    "url": "/static/media/corousal11.05e570b3.jpg"
+  },
+  {
+    "revision": "44b120273120828f1301cf52cb5bf8d8",
+    "url": "/static/media/corousal22.44b12027.jpg"
+  },
+  {
+    "revision": "c1868764dd331af378dd2422ffb0680d",
+    "url": "/static/media/corousal33.c1868764.jpg"
+  },
+  {
+    "revision": "2328ea2a5835b1da4f7fcea251a62377",
+    "url": "/static/media/corousal44.2328ea2a.jpg"
+  },
+  {
+    "revision": "e8f986aca7943f95a083617c369d4be8",
+    "url": "/static/media/logo.e8f986ac.jpg"
+  }
+]);
